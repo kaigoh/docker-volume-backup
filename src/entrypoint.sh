62dab0402f9dd8c827174a399c8a2f6bf1c62d9b
Kai Gohegan <kai@gohegan.uk>
2020-04-06 19:32:45 +0100

    Update entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,7 +7,8 @@ set -e
 cat <<EOF > env.sh
 BACKUP_SOURCES="${BACKUP_SOURCES:-/backup}"
 BACKUP_CRON_EXPRESSION="${BACKUP_CRON_EXPRESSION:-@daily}"
-AWS_S3_BUCKET_NAME="${AWS_S3_BUCKET_NAME:-}"
+S3_ENDPOINT="${S3_ENDPOINT:-}"
+S3_BUCKET_NAME="${S3_BUCKET_NAME:-}"
 BACKUP_FILENAME="$(date +"${BACKUP_FILENAME:-backup-%Y-%m-%dT%H-%M-%S.tar.gz}")"
 BACKUP_ARCHIVE="${BACKUP_ARCHIVE:-/archive}"
 BACKUP_WAIT_SECONDS="${BACKUP_WAIT_SECONDS:-0}"
@@ -24,13 +25,13 @@ source env.sh
 mkdir -p .aws
 cat <<EOF > .aws/credentials
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = ${S3_ACCESS_KEY_ID}
+aws_secret_access_key = ${S3_SECRET_ACCESS_KEY}
 EOF
-if [ ! -z "$AWS_DEFAULT_REGION" ]; then
+if [ ! -z "$S3_DEFAULT_REGION" ]; then
 cat <<EOF > .aws/config
 [default]
-region = ${AWS_DEFAULT_REGION}
+region = ${S3_DEFAULT_REGION}
 EOF
 fi
 
